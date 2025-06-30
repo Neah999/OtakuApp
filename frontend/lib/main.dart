c141899671a81dbb5c7fe61cfcda6cbb484bc2ba
渡瀬孔明 <watasekoumei@watasekoumeiMacBookAir.local>
2025-06-30 19:07:16 +0900

    firebaseの連携

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:frontend/presentation/router/app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     DevicePreview(
       enabled: true,
